28a967d1ae72f58aa04857c962b594942384048b
Jordi Mariné Fort <jmarine@tinet.org>
2016-10-29 11:05:52 +0200

    Perpective far plane was too far away.

--- board3d.js
+++ board3d.js
@@ -24,9 +24,9 @@ app.view.board = (function() {
 
 var board;
 var gl;
-var canvas;
 var redraw;
 var debugShadowBuffer;
+var aspectRatio;
 
 var shadowMatrix;
 var xform;
@@ -224,9 +224,14 @@ Board3D.prototype =
 
         /*************************************************************/
 
+        var canvas = document.getElementById("gameCanvas");
+        var parent = canvas.parentNode;
+        canvas.width = parent.clientWidth;
+        canvas.height = parent.clientHeight;
+        aspectRatio = canvas.width/canvas.height;
         xform = new SglTransformStack();
         xform.projection.loadIdentity();
-        xform.projection.perspective(sglDegToRad(45.0), 1.4, 0.1, 40000.0);
+        xform.projection.perspective(sglDegToRad(45.0), aspectRatio, 0.1, 100.0);
         xform.view.loadIdentity();
         xform.view.lookAt(0.0, -z, z, 0.0,0.0,0.0, 0.0,1.0,0.0);
         xform.model.loadIdentity();
@@ -489,7 +494,7 @@ Board3D.prototype =
         this.dragging = true;
         zRot = rotation;
         if(scale)  z = scale;
-        if(z < 4)  z = 4;
+        if(z < 5)  z = 5;
         if(z > 15) z = 15;
         this.updateRotationMatrix();
         this.invalidate();
@@ -1234,7 +1239,7 @@ Board3D.prototype =
           //biasToLowerMoirePattern = 0.0000160;  // light:[-100,-400,400], distance: 574.45, FOV: 1.044
 
           xform.projection.loadIdentity();
-          xform.projection.perspective(sglDegToRad(fov), 1.0, 0.1,40000.0);
+          xform.projection.perspective(sglDegToRad(fov), aspectRatio, 0.1,100.0);
           xform.view.lookAt(worldLightPos[0],worldLightPos[1],worldLightPos[2], 0.0,0.0,0.0, 0.0,1.0,0.0);
         } else {
           // orthographic projection (to avoid resolution problems with depth values of the shadow map)
@@ -1318,8 +1323,9 @@ Board3D.prototype =
         var parent = canvas.parentNode;
         canvas.width = parent.clientWidth;
         canvas.height = parent.clientHeight;
+	aspectRatio = canvas.width/canvas.height;
         xform.projection.loadIdentity();
-        xform.projection.perspective(sglDegToRad(45.0), canvas.width/canvas.height, 0.1, 40000.0);
+        xform.projection.perspective(sglDegToRad(45.0), aspectRatio, 0.1, 100.0);
         this.invalidate();
         return true;
     }
